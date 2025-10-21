406bc05da6e948a347bb2f7fb699b73b406f6729
Shruti-Sanjeev-Mahale <mahaleshruti786@gmail.com>
2025-10-21 12:26:29 +0530

    Day3

---
+++ Day3/src/com/demo/test/Holiday.java
@@ -0,0 +1,13 @@
+package com.demo.test;
+
+public class Holiday{
+    public static void main(String[] args) {
+        String message = "HOLIDAY";
+
+        System.out.println("*****************");
+        System.out.println("*               *");
+        System.out.println("*   " + message + "   *");
+        System.out.println("*               *");
+        System.out.println("*****************");
+    }
+}
